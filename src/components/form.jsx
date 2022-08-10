04a9a28e0f37b2aad1c962ab0f2fd4da2668887c
umutdurna <umutdurna@sameup.co>
2022-08-10 23:41:25 +0300

    form gelecek kısmı*

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -7,38 +7,10 @@ class Form extends Component {
   render() {
     return (
 
-     <div className="card-body">
       <div>
-        <h1>Kayıt Olun</h1>
+        <p>Kayıt ol formu gelecek</p>
       </div>
-        <form action="GET">
-            <label htmlFor="name">Ad ve Soyadınız</label>
-            <br/>
-            <input type="text" id='adSoyad' />
-            <br/>
-            <label htmlFor="sifre">Bir şifre belirleyiniz</label>
-            <br/>
-            <label htmlFor="Çocuğun ismi?"></label>
-            <br />
-            <br/>
-            <input type="text" id='ismi' />
-            <br/>
-            <label htmlFor="Çocuğunuzun Cinsiyet?">Çocuğunuzun Cinsiyeti</label>
-            <br />
-            <br/>
-            <label htmlFor="erkek">Erkek</label>
-            <input type="radio" />
-            <label htmlFor="kadın">Kadın</label>
-            <input type="radio" />
-            <br/>
-            <br/>
-            <label htmlFor="Çocuğun İsmi?">Çocuğun İsmi?</label>
-            <br />
-            <br />
-            <input type='text' id='cocugunismi' />
-
-        </form>
-     </div>
+    
 
     )
   }
